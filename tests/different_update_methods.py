e92a2beb54e7f57218a56251ca203ae63ee7567d
denet <deneth@hotmail.co.uk>
2021-12-04 01:34:44 +0000

    minor changes

--- tests/different_update_methods.py
+++ tests/different_update_methods.py
@@ -9,7 +9,7 @@ new_path = os.path.join(os.path.dirname(os.getcwd()), "data_files")
 def retrieve_data(file, s):
     """
     :param file: .npy file to extract data from
-    :param s: which type of data to extract. 0 is time and 1-n is a Particle instance
+    :param s: which type of data to extract. 0 is time and 1-n is a Particle object
     :return: the times and positions extracted
     """
     data = np.load(file, allow_pickle=True)
@@ -21,7 +21,7 @@ def retrieve_data(file, s):
     return times, positions
 
 
-sat_1_times, sat_1_pos = retrieve_data(new_path + "/two_body_test.npy", 2)
+sat_1_times, sat_1_pos = retrieve_data(new_path + "/two_body_test.npy", 2)  # 2 for the second object i.e. the satellite
 sat_2_times, sat_2_pos = retrieve_data(new_path + "/cromer_test.npy", 2)
 
 euler_x_y = SimpleGraphPlot(sat_1_times, sat_1_pos)  # Plot for the Euler method
