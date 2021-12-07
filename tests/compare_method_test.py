fixed error in the calculation of position within updateVerler

tests/compare_method_test.py
@@ -9,15 +9,16 @@ new_path = os.path.join(os.path.dirname(os.getcwd()), "data_files")
 def retrieve_data(file):
     raw_data = np.load(new_path + file, allow_pickle=True)
     processed_data = []
-    names = []
+    labels = []
+    print(raw_data[0][])
     for i in raw_data:
         temp = []
         for j in range(0, len(i)):
-            if len(names) < len(i):  # generates separate lists for the names and colours of the objects
-                names.append(i[j].name)
+            if len(labels) < len(i):  # generates separate lists for the names and colours of the objects
+                labels.append(i[j].name)
             temp.append(i[j].position)
         processed_data.append(temp)
-    return processed_data, names
+    return processed_data, labels
 
 
 def plot_paths(data, labels):
@@ -34,6 +35,6 @@ def plot_paths(data, labels):
 
 
 dataset, names = retrieve_data("/method_test_data.npy")
-print(dataset[1])
-# plot_paths(dataset, names)
+# print(dataset[1])
+plot_paths(dataset, names)
 