a868adce6526c37570cd751462ce86268ae24054
denet <deneth@hotmail.co.uk>
2021-12-07 15:24:03 +0000

    Added docstrings to functions

--- tests/compare_method_test.py
+++ tests/compare_method_test.py
@@ -7,10 +7,16 @@ new_path = os.path.join(os.path.dirname(os.getcwd()), "data_files")
 
 
 def retrieve_data(file):
+    """
+    Reads data from the selected data file, obtaining data useful for plotting
+    :param file: string; filename
+    :return: 3D list containing the 3-vector positions of each object as floats at each time and a list containing the
+    string names of each object
+    """
     raw_data = np.load(new_path + file, allow_pickle=True)
     processed_data = []
     labels = []
-    print(raw_data[0][])
+    # print(raw_data[0][3])
     for i in raw_data:
         temp = []
         for j in range(0, len(i)):
@@ -22,6 +28,11 @@ def retrieve_data(file):
 
 
 def plot_paths(data, labels):
+    """
+    Plots and displays the data as a series of overlaid 2D graphs of y-position against x-position
+    :param data: 3D list of floats; the input data
+    :param labels:
+    """
     for p in range(len(data[0])):  # iterates through each particle
         subset = []
         for i in data:  # looking at ith time saved
@@ -36,5 +47,5 @@ def plot_paths(data, labels):
 
 dataset, names = retrieve_data("/method_test_data.npy")
 # print(dataset[1])
-plot_paths(dataset, names)
+# plot_paths(dataset, names)
 
